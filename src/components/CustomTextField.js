3ff3464fa868e05659b52dc53d432cbf85223f3a
Huy Truong <ht@HMPB.local>
2020-09-24 19:26:44 -0700

    Custom Disabled TextField

---
+++ src/components/CustomTextField.js
@@ -0,0 +1,19 @@
+import { 
+  TextField,
+  withStyles,
+} from "@material-ui/core";
+
+const CustomTextField = withStyles({
+  root: {
+    margin: "10px 10px",
+    variant: "outlined",
+    "& .MuiInputBase-root.Mui-disabled": {
+      color: "black"
+    },
+    "& .MuiFormLabel-root.Mui-disabled": {
+      color: "black"
+    }
+  }
+})(TextField);
+
+export default CustomTextField;
