67a148c449c430be8e99f8c415119678b348a02d
Huy Truong <ht@HMPB.local>
2020-09-26 16:20:11 -0700

    import css

--- src/components/StudentOne/StudentOne.js
+++ src/components/StudentOne/StudentOne.js
@@ -2,7 +2,6 @@ import CustomTextField from '../CustomFields/CustomTextField';
 import React from 'react';
 import './StudentOne.css';
 
-
 const studentOne = (props) => {
   const { studentOne } = props;
 
